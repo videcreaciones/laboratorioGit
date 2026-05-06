030d16955b2b4689707c97cc0c4c4593648da3f9
Juanaki <nicopan120@gmail.com>
2026-05-06 08:41:15 -0500

    Hola 1
    Added nested loops to print asterisks in a grid format.

--- labgit.cpp
+++ labgit.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
-	
-	cout<<"hola grupo";
+    int filas = 3;
+
+    for (int i = 0; i < filas; i++) {        
+        for (int j = 0; j < filas; j++) {   
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 	
 }
