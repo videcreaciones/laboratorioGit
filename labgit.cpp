83c2fd0a0b5585a52b55246c85e89395fad9cd0f
Gonzoan <dugonzalezr@unal.edu.co>
2026-05-09 08:58:38 -0500

    Se modifico el codigo como corresponde segun el orden del taller

--- labgit.cpp
+++ labgit.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
 using namespace std;
-
 int main(){
-    int filas = 3;
-
-    for (int i = 0; i < filas; i++) {        
-        for (int j = 0; j < filas; j++) {   
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-	
+    
+    
+    
+	return 0;
 }
