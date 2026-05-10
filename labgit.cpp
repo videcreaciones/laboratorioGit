e316bf90419683e3c3c0c645c6b89f01b603139a
unknown <nicopan120@gmail.com>
2026-05-10 11:41:13 -0500

    mensaje

--- labgit.cpp
+++ labgit.cpp
@@ -1,25 +1,23 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<sstream>
 
 using namespace std;
 
 int main() {
 
-    ifstream archivoEntrada("input/ArchivoEntrada.txt");
-	ofstream archivoSalida("output/ArchivoSalida.txt");
+    ifstream archivoEntrada("input/archivoEntrada.txt");
+    ofstream archivoSalida("output/archivoSalida.txt");
 
-    if (archivoEntrada.is_open() && archivoSalida.is_open()){
+    if (archivoEntrada.is_open() && archivoSalida.is_open()) {
         cout << "Archivo abierto" << endl;
 
         string linea;
 
-        while (getline(archivoEntrada, linea)) { + }
+        while (getline(archivoEntrada, linea)) {
             archivoSalida << linea;
         }
-        cout << "Archivo de salida copiado correctamente " << endl;
-        archivoEntrada.close();
-    }
 
-    return 0;
+        cout << "Archivo copiado correctamente" << endl;
+    }
 }
