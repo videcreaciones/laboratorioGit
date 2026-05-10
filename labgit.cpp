fcdb4fc08122019f984b11a27a4bdee118e6b6f0
Salomon Jarro <chovengo2018@gmail.com>
2026-05-09 23:01:02 -0500

    se agregaron las lineas de texto y el codigo necesario para leerlo

--- labgit.cpp
+++ labgit.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <fstream>
+#include <string>
 
 using namespace std;
-int main(){
-    
-    
-    
-	return 0;
+
+int main() {
+
+    ifstream archivoEntrada("input/Archivodetexto.txt");
+
+    if (archivoEntrada.is_open()) {
+
+        cout << "Archivo abierto" << endl;
+
+        string lineas;
+
+        while (getline(archivoEntrada, lineas)) {
+            cout << lineas << endl;
+        }
+
+        archivoEntrada.close();
+    }
+
+    return 0;
 }
